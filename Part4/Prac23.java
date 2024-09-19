418422ae7d17c31dd81d5540311ca570b11758c1
AryaPatel25 <91053876+AryaPatel25@users.noreply.github.com>
2024-09-19 13:03:39 +0530

    Update Prac23.java

--- Part4/Prac23.java
+++ Part4/Prac23.java
@@ -1,16 +1,22 @@
-interface Shape {
-    String getColor();
-   
-    default String describe() {
-        return "This is a shape with color " + getColor();
-    }
-}
- class Circle implements Shape {
-    private double radius;
-    private String color;
+import java.util.Scanner;
 
-    public Circle(double radius, String color) {
-        this.radius = radius;
+interface Shape {
+    public String shapeName = "";
+    public void setColor(String color);
+    public String getColor();
+    public void display();
+}
+
+class Circle implements Shape {
+    protected String shapeName = "Circle";
+    protected double radius;
+    protected String color;
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
         this.color = color;
     }
 
@@ -18,24 +24,32 @@ interface Shape {
         return radius;
     }
 
-   
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+
+    public String getShapeName() {
+        return shapeName;
+    }
+
+    public void display() {
+        System.out.println("NAME:" + getShapeName());
+        System.out.println("COLOR:" + getColor());
+        System.out.println("RADIUS:" + getRadius());
+    }
+}
+
+class Rectangle implements Shape {
+    protected String shapeName = "Rectangle";
+    protected double length;
+    protected double breadth;
+    protected String color;
+
     public String getColor() {
         return color;
     }
 
- 
-    public String describe() {
-        return "This is a circle with radius " + radius + " and color " + color;
-    }
-}
- class Rectangle implements Shape {
-    private double length;
-    private double width;
-    private String color;
-
-    public Rectangle(double length, double width, String color) {
-        this.length = length;
-        this.width = width;
+    public void setColor(String color) {
         this.color = color;
     }
 
@@ -43,47 +57,77 @@ interface Shape {
         return length;
     }
 
-    public double getWidth() {
-        return width;
+    public void setLength(double length) {
+        this.length = length;
     }
 
-
-    public String getColor() {
-        return color;
+    public double getBreadth() {
+        return breadth;
     }
 
-
-    public String describe() {
-        return "This is a rectangle with length " + length + ", width " + width + " and color " + color;
-    }
-}
- class Sign {
-    private Shape shape;
-    private String text;
-
-    public Sign(Shape shape, String text) {
-        this.shape = shape;
-        this.text = text;
+    public void setBreadth(double breadth) {
+        this.breadth = breadth;
     }
 
-    public void displaySign() {
-        System.out.println(shape.describe() + ". Text on the sign: " + text);
+    public String getShapeName() {
+        return shapeName;
+    }
+
+    public void display() {
+        System.out.println("NAME:" + getShapeName());
+        System.out.println("COLOR:" + getColor());
+        System.out.println("LENGTH:" + getLength());
+        System.out.println("BREADTH: " + getBreadth());
     }
 }
- class Prac23 {
+
+class sign {
+    Scanner in = new Scanner(System.in);
+    private String t;
+ 
+    public void print() {
+       System.out.println("ENTER SHAPE [1. RACTANGLE 2. CIRCLE] : ");
+       int n = in.nextInt();
+       Rectangle r = new Rectangle();
+       Circle c = new Circle();
+ 
+       if (n == 1) {
+          System.out.println("ENTER COLOR : ");
+          r.setColor(in.next());
+          System.out.println("ENTER HEIGHT : ");
+          r.setLength(in.nextInt());
+          System.out.println("ENTER WIDTH : ");
+          r.setBreadth(in.nextInt());
+ 
+       } else {
+          System.out.println("ENTER COLOR : ");
+          c.setColor(in.next());
+          System.out.println("ENTER RADIUS : ");
+          c.setRadius(in.nextInt());
+ 
+       }
+       System.out.println("ENTER TEXT : ");
+       in.nextLine();
+       t = in.nextLine();
+ 
+       if (n == 1) {
+          System.out.println("SIGN DETAIL :- ");
+          r.display();
+          System.out.println(t);
+       } else {
+          System.out.println("SIGN DETAIL :- ");
+          c.display();
+          System.out.println(t);
+       }
+       in.close();
+    }
+ }
+
+ public class prac_23 {
     public static void main(String[] args) {
-       
-        Circle circle = new Circle(5.0, "Red");
+        Scanner in = new Scanner(System.in);
 
-       
-        Rectangle rectangle = new Rectangle(4.0, 6.0, "Blue");
-
-
-        Sign circleSign = new Sign(circle, "DEPSTAR");
-        Sign rectangleSign = new Sign(rectangle, "CHARUSAT");
-
-
-        circleSign.displaySign();
-        rectangleSign.displaySign();
+        sign s = new sign();
+        s.print();
     }
-}
+ }
